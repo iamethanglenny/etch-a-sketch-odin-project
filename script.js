fcabd0be5ead842c35ba4afc50288c536095dd5f
iamethanglenny <ethanglenny@gmail.com>
2024-10-18 12:37:27 +0200

    Feat: Add styling to html and body, and attempts to get grid-squares to show up on page -- height formation seems to be the issue

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ function createGrid(rows, cols) {
     const gridContainer = document.getElementById("grid-container");
     gridContainer.innerHTML = '';
 
-    const squareWidth = 100 / cols;
-    const squareHeight = 100 / rows;
+    
 
     for (let i = 0; i < rows * cols; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
 
-        gridSquare.style.flex = `1 0 calc(${squareWidth})% - 2px)`;
-        gridSquare.style.height = `calc(${squareWidth})vh - 2px)`;
+        gridSquare.style.flex = `1 0 calc(${100 / cols})% - 2px)`;
+        gridSquare.style.height = `calc(${100 / rows})vh - 2px)`;
 
         gridSquare.addEventListener("mouseover", () => {
             gridSquare.style.backgroundColor = getRandomColor();
         })
+        console.log("Creating square", i);
     
         gridContainer.appendChild(gridSquare);
     }
@@ -31,14 +31,13 @@ function createGrid(rows, cols) {
 
 
 // GET user input for a new grid based on their sizing choice
-document.getElementById('grid-form').addEventListener("submit", function(event) => {
+document.getElementById('grid-form').addEventListener("submit", function(event) {
     event.preventDefault();
-    while (rows < 100 || cols < 100) {
+
         const rows = document.getElementById("rows").value;
         const cols = document.getElementById("cols").value;
 
         createGrid(rows, cols);
-    }
 })
 
 // INITIALIZE page with a 16 x 16 grid
